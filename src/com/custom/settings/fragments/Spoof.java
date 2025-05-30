213d84b67cfa39566b51b834bbe65e29526c8941
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-05-31 06:49:32 +0800

    [Spoof] removal of play integrity spoofing
    Change-Id: I424a88602bca34ad5c378e763b515aeca718d6b2

--- src/com/custom/settings/fragments/Spoof.java
+++ src/com/custom/settings/fragments/Spoof.java
@@ -15,17 +15,10 @@
  */
 package com.custom.settings.fragments;
 
-import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.SystemProperties;
-import android.text.TextUtils;
 import android.util.Log;
-import android.widget.Toast;
 
 import androidx.preference.Preference;
 
@@ -36,215 +29,28 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.Iterator;
 import java.util.List;
 
 @SearchIndexable
 public class Spoof extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
 
     public static final String TAG = "Spoof";
-    private static final String SYS_GMS_SPOOF = "persist.sys.pixelprops.gms";
     private static final String SYS_GPHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
-    private static final String KEY_PIF_JSON_FILE_PREFERENCE = "pif_json_file_preference";
-    private static final String KEY_UPDATE_JSON_BUTTON = "update_pif_json";
 
-    private Preference mGmsSpoof;
     private Preference mGphotosSpoof;
-    private Preference mPifJsonFilePreference;
-    private Preference mUpdateJsonButton;
-
-    private Handler mHandler;
-    
-    private static final String[] PIF_KEYS = {
-        "ID",
-        "BRAND",
-        "DEVICE",
-        "FINGERPRINT",
-        "MANUFACTURER",
-        "MODEL",
-        "PRODUCT",
-        "SECURITY_PATCH",
-        "DEVICE_INITIAL_SDK_INT",
-        "TYPE",
-        "TAGS",
-        "RELEASE",
-        "DEBUG",
-        "SDK_INT"
-    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mHandler = new Handler();
         addPreferencesFromResource(R.xml.spoof);
 
         mGphotosSpoof = findPreference(SYS_GPHOTOS_SPOOF);
-        mGmsSpoof = findPreference(SYS_GMS_SPOOF);
-        mPifJsonFilePreference = findPreference(KEY_PIF_JSON_FILE_PREFERENCE);
-        mUpdateJsonButton = findPreference(KEY_UPDATE_JSON_BUTTON);
-
-        mGmsSpoof.setOnPreferenceChangeListener(this);
         mGphotosSpoof.setOnPreferenceChangeListener(this);
-
-        mPifJsonFilePreference.setOnPreferenceClickListener(preference -> {
-            openFileSelector(10001);
-            return true;
-        });
-
-        mUpdateJsonButton.setOnPreferenceClickListener(preference -> {
-            updatePropertiesFromUrl("https://raw.githubusercontent.com/AxionAOSP/PlayIntegrityFix/refs/heads/lineage-22.1/pif.json");
-            return true;
-        });
-
-        Preference showPropertiesPref = findPreference("show_pif_properties");
-        if (showPropertiesPref != null) {
-            showPropertiesPref.setOnPreferenceClickListener(preference -> {
-                showPropertiesDialog();
-                return true;
-            });
-        }
-    }
-
-    private void openFileSelector(int requestCode) {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("application/json");
-        startActivityForResult(intent, requestCode);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == Activity.RESULT_OK && data != null) {
-            Uri uri = data.getData();
-            if (uri != null) {
-                if (requestCode == 10001) {
-                    loadPifJson(uri);
-                }
-            }
-        }
-    }
-
-    private void showPropertiesDialog() {
-        StringBuilder properties = new StringBuilder();
-        try {
-            JSONObject jsonObject = new JSONObject();
-            String[] keys = {
-                "persist.sys.pihooks_ID",
-                "persist.sys.pihooks_BRAND", 
-                "persist.sys.pihooks_DEVICE",
-                "persist.sys.pihooks_FINGERPRINT",
-                "persist.sys.pihooks_MANUFACTURER",
-                "persist.sys.pihooks_MODEL",
-                "persist.sys.pihooks_PRODUCT",
-                "persist.sys.pihooks_SECURITY_PATCH",
-                "persist.sys.pihooks_DEVICE_INITIAL_SDK_INT",
-                "persist.sys.pihooks_TYPE",
-                "persist.sys.pihooks_TAGS",
-                "persist.sys.pihooks_RELEASE",
-                "persist.sys.pihooks_DEBUG",
-                "persist.sys.pihooks_SDK_INT"
-            };
-            for (String key : keys) {
-                String value = SystemProperties.get(key, null);
-                if (!TextUtils.isEmpty(value)) {
-                    String buildKey = key.replace("persist.sys.pihooks_", "");
-                    jsonObject.put(buildKey, value);
-                }
-            }
-            String jsonString = jsonObject.toString(4);
-            jsonString = jsonString.replace("\\/", "/");
-            properties.append(jsonString);
-        } catch (JSONException e) {
-            Log.e(TAG, "Error creating JSON from properties", e);
-            properties.append(getString(R.string.error_loading_properties));
-        }
-        new AlertDialog.Builder(getContext())
-            .setTitle(R.string.show_pif_properties_title)
-            .setMessage(properties.toString())
-            .setPositiveButton(android.R.string.ok, null)
-            .show();
-    }
-
-    private void updatePropertiesFromUrl(String urlString) {
-        new Thread(() -> {
-            try {
-                URL url = new URL(urlString);
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                try (InputStream inputStream = urlConnection.getInputStream()) {
-                    String json = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
-                    Log.d(TAG, "Downloaded JSON data: " + json);
-                    JSONObject jsonObject = new JSONObject(json);
-                    String spoofedModel = jsonObject.optString("MODEL", "Unknown model");
-                    for (Iterator<String> it = jsonObject.keys(); it.hasNext(); ) {
-                        String key = it.next();
-                        String value = jsonObject.getString(key);
-                        Log.d(TAG, "Setting property: persist.sys.pihooks_" + key + " = " + value);
-                        SystemProperties.set("persist.sys.pihooks_" + key, value);
-                    }
-                    for (String pifKey : PIF_KEYS) {
-                        if (!jsonObject.has(pifKey)) {
-                            Log.d(TAG, "Clearing unspecified property: " + pifKey);
-                            SystemProperties.set("persist.sys.pihooks_" + pifKey, "");
-                        }
-                    }
-                    mHandler.post(() -> {
-                        String toastMessage = getString(R.string.toast_spoofing_success, spoofedModel);
-                        Toast.makeText(getContext(), toastMessage, Toast.LENGTH_LONG).show();
-                    });
-                } finally {
-                    urlConnection.disconnect();
-                }
-            } catch (Exception e) {
-                Log.e(TAG, "Error downloading JSON or setting properties", e);
-                mHandler.post(() -> {
-                    Toast.makeText(getContext(), R.string.toast_spoofing_failure, Toast.LENGTH_LONG).show();
-                });
-            }
-            mHandler.postDelayed(() -> {
-                SystemRestartUtils.showSystemRestartDialog(getContext());
-            }, 1250);
-        }).start();
-    }
-
-    private void loadPifJson(Uri uri) {
-        Log.d(TAG, "Loading PIF JSON from URI: " + uri.toString());
-        try (InputStream inputStream = getActivity().getContentResolver().openInputStream(uri)) {
-            if (inputStream != null) {
-                String json = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
-                Log.d(TAG, "PIF JSON data: " + json);
-                JSONObject jsonObject = new JSONObject(json);
-                for (Iterator<String> it = jsonObject.keys(); it.hasNext(); ) {
-                    String key = it.next();
-                    String value = jsonObject.getString(key);
-                    Log.d(TAG, "Setting PIF property: persist.sys.pihooks_" + key + " = " + value);
-                    SystemProperties.set("persist.sys.pihooks_" + key, value);
-                }
-                for (String pifKey : PIF_KEYS) {
-                    if (!jsonObject.has(pifKey)) {
-                        Log.d(TAG, "Clearing unspecified property: " + pifKey);
-                        SystemProperties.set("persist.sys.pihooks_" + pifKey, "");
-                    }
-                }
-            }
-        } catch (Exception e) {
-            Log.e(TAG, "Error reading PIF JSON or setting properties", e);
-        }
-        mHandler.postDelayed(() -> {
-            SystemRestartUtils.showSystemRestartDialog(getContext());
-        }, 1250);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mGmsSpoof
-            || preference == mGphotosSpoof) {
+        if (preference == mGphotosSpoof) {
             SystemRestartUtils.showSystemRestartDialog(getContext());
             return true;
         }
